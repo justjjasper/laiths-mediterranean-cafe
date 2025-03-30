48a5f2333a9b483f799f43cc31b12f531922d0f1
JASPER <jasjasper101@gmail.com>
2025-03-30 16:42:04 -0700

    Implement brown svg bg at the bottom of Our Story

--- app/components/OurStory/OurStory.js
+++ app/components/OurStory/OurStory.js
@@ -10,7 +10,7 @@ const ContainerSection = tw.section`
   flex-col md:flex-row
   bg-main-color
   px-5 xsm:px-10 sm:px-5 lg:px-16
-  pb-10 md:pb-20
+  pb-16 md:pb-20
 `;
 
 const Content = tw(motion.div)`
@@ -24,19 +24,20 @@ const H2 = tw.h2`
 
 const P = tw.p`
   text-lg font-epilogue
-   md:w-[65%]
+  md:w-[65%]
 `
 
 const MotionImage = tw(Image)`
   h-[25rem] w-[25rem]
   lg:h-[30rem] lg:w-[30rem]
-  rounded-lg
+  rounded-xl
+  pb-10 md:pb-0
 `;
 
 //! ---> COMPONENT <---
 export default function OurStory() {
   return (
-    <ContainerSection id='our-story' className='scroll-smooth'>
+    <ContainerSection id='our-story' className='scroll-smooth relative'>
       <Content>
         <H2>Our Story</H2>
         <P>Experience authentic Mediterranean flavors with every bite at My Mediterranean Restaurant. Join us in Carson, CA today!</P>
@@ -47,6 +48,14 @@ export default function OurStory() {
         width={200}
         height={200}
       />
+      <svg
+        className="absolute bottom-0 left-0 w-full h-24"
+        viewBox="0 0 1440 100"
+        preserveAspectRatio="none"
+        fill="#5F3C1b"
+      >
+        <path d="M0,0 C720,100 720,100 1440,30 L1440,100 L0,100 Z" />
+      </svg>
     </ContainerSection>
   );
 }
